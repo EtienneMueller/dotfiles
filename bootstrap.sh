b53861b6c09be013bcae8e2d4a9c4d9847de348d
Etienne Mueller <etienne.mueller@outlook.com>
2026-06-20 12:17:27 +0700

    updated bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Xcode Command Line Tools
 xcode-select --install
 
-# Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-# Packages
 brew install git wget
 brew install --cask firefox
 brew install --cask obsidian
