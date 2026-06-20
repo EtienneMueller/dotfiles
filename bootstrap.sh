6aa79314330a228d331e4c5ffd38ea6dae2687ca
Etienne Mueller <etienne.mueller@outlook.com>
2026-06-20 09:51:50 +0700

    added initial bootstrap.sh script

---
+++ bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Xcode Command Line Tools
+xcode-select --install
+
+# Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Packages
+brew install git wget
+brew install --cask firefox
